8225b2405fca03945a9a83413700046fcbb4d40d
franck <franck.rst@gmail.com>
2020-09-02 16:55:34 +0200

    WIP

--- ci/run-local.sh
+++ ci/run-local.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 echo "==============================="
-#envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" < \
-#  /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && \
-#  nginx
-/usr/local/nginx/sbin/nginx -g deamon off;
+/usr/local/nginx/sbin/nginx
